d6ebea864fe527a5303a5439514856397b1074a9
Martho Evan <martho.evan@gmail.com>
2024-01-17 13:27:41 +0100

    Initial Commit

---
+++ SwiftUIApiRest/SwiftUIApiRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApiRestApp.swift
+//  SwiftUIApiRest
+//
+//  Created by Evan Martho on 17/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApiRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
